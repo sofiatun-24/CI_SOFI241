bac6146c2790f2536080201505108799d2d0228e
sofiatun-24 <ntiieee3@gmail.com>
2026-06-09 15:24:58 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	   => '',
-	'hostname' => 'localhost',
+	'hostname' => 'localhost:4307',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'db_salesorder',
