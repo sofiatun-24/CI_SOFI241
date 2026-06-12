a053d03c976b0860b0c3da965f96e7e5092c40e6
sofiatun-24 <ntiieee3@gmail.com>
2026-06-12 23:41:47 +0700

    update

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -53,7 +53,7 @@
     <?php endif; ?>
 
     <!-- ADMIN & SALES: Transaksi -->
-    <?php if (in_array($this->session->userdata('role'), ['admin', 'pelanggan'])): ?>
+    <?php if (in_array($this->session->userdata('role'), ['admin', 'sales'])): ?>
 
         <hr class="sidebar-divider">
         <div class="sidebar-heading">Transaksi</div>
